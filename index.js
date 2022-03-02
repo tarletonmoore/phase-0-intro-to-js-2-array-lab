f8b1f7e76dce057a3d74bb12060e53a07d0b9578
John Tarleton Moore <tarletonmoore@gmail.com>
2022-03-02 14:57:06 -0500

    commit tree d1bdf52133a54ca074b6eb477ca4b78e2fc93c5b

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = [`Milo`, `Otis`, `Garfield`]
+
+function destructivelyAppendCat() {
+    cats.push(`Ralph`)
+    return cats
+}
+
+function destructivelyPrependCat() {
+    cats.unshift(`Bob`)
+    return cats
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(2)
+    return cats
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0)
+    return cats
+}
+
+function appendCat() {
+    const copyOfCats = [...cats, `Broom`]
+    return copyOfCats
+}
+
+function prependCat() {
+    const copyOfCats = [`Arnold`, ...cats]
+    return copyOfCats
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(0, -1)
+    return copyOfCats
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1)
+    return copyOfCats
+}
